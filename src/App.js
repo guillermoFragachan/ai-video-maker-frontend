8ef130c26bdb121b1de1af2fc95bdb78a011ec3a
Guilermo Fragachan <guillefragachan@gmail.com>
2024-09-17 20:14:59 +0200

    install prime

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/saga-blue/theme.css';  // or choose any other theme
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css'; // for icons
+import 'primeflex/primeflex.css'; // for layout utilities
+import './index.css'; // keep this for your custom styles if needed
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-d-flex p-ai-center p-jc-center min-h-screen">
+      <Button label="Click Me" icon="pi pi-check" className="p-button-raised p-button-success" />
     </div>
   );
 }
+
+
+
 
 export default App;
