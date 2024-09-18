3f794cdb84c9cd920cbeb99f79fb2dce4945d4ad
Guilermo Fragachan <guillefragachan@gmail.com>
2024-09-18 16:08:37 +0200

    chat done

--- src/App.js
+++ src/App.js
@@ -1,61 +1,33 @@
 import React, {useEffect, useState} from 'react';
 import { Button } from 'primereact/button';
-import 'primereact/resources/themes/saga-blue/theme.css';  // or choose any other theme
+import 'primereact/resources/themes/saga-blue/theme.css'; 
 import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css'; // for icons
-import 'primeflex/primeflex.css'; // for layout utilities
-import './index.css'; // keep this for your custom styles if needed
-
+import 'primeicons/primeicons.css'; 
+import 'primeflex/primeflex.css'; 
+import './index.css'; 
+import { Chat } from './pages/chat';
 
 
 function App() {
 
 
-  const [name, setName] = useState(false)
-  const [isApproved, seIsApproved] = useState(false)
 
-
-
-  const [users, setUsers] = useState([])
-
-
-  const getUsers =() =>  {
-
-    return ['userio 1', '2', '2']
-  }
-
-  useEffect(()=> {
-
-    const list = getUsers()
-
-    setUsers(list)
- 
-  }, [ ])
   return (
-    <div className="p-d-flex p-ai-center p-jc-center min-h-screen">
+    <div className="p-d-flex p-ai-center p-jc-center min-h-screen" style={{}}>
 
-      <button onClick={()=>{
-        seIsApproved(true)
-      }} > change name  </button>
+<div className="p-d-flex p-jc-center p-ai-center" style={{ height: '100vh', backgroundColor: '#1e1e1e', display:"flex"  }}>
 
-      {users.map((user)=> {
+      <div style={{
+        width:"20%"
+      }}></div>
+      <Chat /> 
+      </div>
 
-
-
-return <div>{user}</div>
-      })}
-      <Complement name={name}/>
-    </div>
+      </div>
   );
 }
 
 
-function Complement(props) { 
-
-
-  return       <Button label={props.name} icon="pi pi-check" className="p-button-raised p-button-success" />
-
-}
 
 
 
